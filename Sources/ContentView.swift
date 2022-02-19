dd2709695bff3d5a09b79ac00c96e154de701c8a
dochoi <dochoi@student.42seoul.kr>
2022-02-19 16:09:31 +0900

    contentView 추가

---
+++ Sources/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Hagomandal
+//
+//  Created by 최동규 on 2022/02/19.
+//  Copyright © 2022 NxMachineCorps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello Robot")
+    }
+}
